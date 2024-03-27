c43aee2d61417dc0877afc5909baed32ef5abf58
Morgan Du <morgandu@google.com>
2024-03-27 18:39:06 +0000

    hardcode 1 thread

--- MaxText/maxengine_server.py
+++ MaxText/maxengine_server.py
@@ -41,7 +41,7 @@ def main(config):
   # local credentials.
   # TODO: Add grpc credentials for OSS.
   jetstream_server = server_lib.run(
-      threads=256,
+      threads=1,
       port=9000,
       config=server_config,
       devices=devices,
